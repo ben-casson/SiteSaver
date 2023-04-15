d0befbe7692117786afa8e1e39aa8a448717f44b
Ben Casson <bcasson11@gmail.com>
2023-04-15 17:30:33 -0500

    Create component for custom options elements

---
+++ front-end/components/MenuItem.tsx
@@ -0,0 +1,24 @@
+import { Dispatch, SetStateAction } from 'react';
+import styles from './MenuItem.module.scss';
+
+type MenuItemProps = {
+    name: string;
+    theme?: string;
+    setCurrentTheme: Dispatch<SetStateAction<string>>;
+};
+
+export function MenuItem({ name, theme, setCurrentTheme }: MenuItemProps) {
+    const toggleTheme = (event: any) => {
+        const dataTheme = event.target.getAttribute('data-theme');
+        document.body.className = dataTheme;
+        setCurrentTheme(name);
+    };
+
+    return (
+        <div className={styles.menuItemContainer} role='menuitem' tabIndex={-1}>
+            <button className={styles.themeButton} onClick={toggleTheme} data-theme={theme}>
+                {name}
+            </button>
+        </div>
+    );
+}
