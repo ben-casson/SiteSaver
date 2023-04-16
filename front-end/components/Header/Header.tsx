ba42e2b29d1851a84eb411fdb7f7369068da7367
Ben Casson <bcasson11@gmail.com>
2023-04-16 12:08:01 -0500

    Create header component

--- front-end/components/Header/Header.tsx
+++ front-end/components/Header/Header.tsx
@@ -1,7 +1,25 @@
-export default function Header() {
+import styles from './Header.module.scss';
+import Image from 'next/image';
+import darkModeIcon from '../../public/bookmark-dark-mode.png';
+import lightModeIcon from '../../public/bookmark-light-mode.png';
+
+type Props = {
+    currentTheme: string;
+};
+
+export default function Header({ currentTheme }: Props) {
     return (
-        <header>
-            <div></div>
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <Image
+                    src={currentTheme === 'Light' ? lightModeIcon : darkModeIcon}
+                    alt='bookmark icon'
+                    className={styles.logo}
+                    width={60}
+                    height={60}
+                    priority
+                />
+            </div>
         </header>
     );
 }
