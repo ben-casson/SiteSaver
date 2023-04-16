983725c18fcfc80bcd0b01df3e67e527da3c59e1
Ben Casson <bcasson11@gmail.com>
2023-04-16 13:48:20 -0500

    Replace MenuItems with children prop

--- front-end/components/Menu/Menu.tsx
+++ front-end/components/Menu/Menu.tsx
@@ -1,15 +1,14 @@
 import styles from './Menu.module.scss';
-import { Dispatch, SetStateAction } from 'react';
 import { MenuButton } from './MenuButton';
-import { MenuItem } from './MenuItem';
 import useComponentVisible from '@/hooks/useComponentVisible';
 
 type Props = {
-    currentTheme: string;
-    setCurrentTheme: Dispatch<SetStateAction<string>>;
+    children: any;
+    label: string;
+    selectedOption: string;
 };
 
-export function Menu({ currentTheme, setCurrentTheme }: Props) {
+export function Menu({ children, label, selectedOption }: Props) {
     const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(
         false,
         toggleMenu
@@ -26,19 +25,14 @@ export function Menu({ currentTheme, setCurrentTheme }: Props) {
         <>
             <div id={styles.container} ref={ref}>
                 <MenuButton
+                    label={label}
                     toggleMenu={toggleMenu}
                     menuIsOpen={isComponentVisible}
-                    currentTheme={currentTheme}
+                    selectedOption={selectedOption}
                 />
                 {isComponentVisible && (
                     <div className={styles.menuItems} id='menu-items' role='menu' tabIndex={0}>
-                        <MenuItem name='Light' theme='light' setCurrentTheme={setCurrentTheme} />
-                        <MenuItem name='Dark' theme='dark' setCurrentTheme={setCurrentTheme} />
-                        <MenuItem
-                            name='Flat Dark'
-                            theme='flat-dark'
-                            setCurrentTheme={setCurrentTheme}
-                        />
+                        {children}
                     </div>
                 )}
             </div>
