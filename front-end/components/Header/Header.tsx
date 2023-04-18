5a8321f0a4d75463a591846c90352dd9b3b68b64
Ben Casson <bcasson11@gmail.com>
2023-04-17 20:48:23 -0500

    Use Zustand theme state

--- front-end/components/Header/Header.tsx
+++ front-end/components/Header/Header.tsx
@@ -2,22 +2,20 @@ import styles from './Header.module.scss';
 import Image from 'next/image';
 import darkModeIcon from '../../public/bookmark-dark-mode.png';
 import lightModeIcon from '../../public/bookmark-light-mode.png';
+import useThemeStore from '@/lib/themeStore';
 
-type Props = {
-    currentTheme: string;
-};
+export default function Header() {
+    const theme = useThemeStore((state) => state.theme);
 
-export default function Header({ currentTheme }: Props) {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
                 <Image
-                    src={currentTheme === 'Light' ? lightModeIcon : darkModeIcon}
+                    src={theme === 'Light' ? lightModeIcon : darkModeIcon}
                     alt='bookmark icon'
                     className={styles.logo}
                     width={60}
                     height={60}
-                    priority
                 />
             </div>
         </header>
