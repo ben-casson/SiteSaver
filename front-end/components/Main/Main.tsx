6333976a539a002917d7c0e747576ee755dea5c7
Ben Casson <bcasson11@gmail.com>
2023-04-17 20:48:49 -0500

    Remove useless code

--- front-end/components/Main/Main.tsx
+++ front-end/components/Main/Main.tsx
@@ -1,16 +1,10 @@
 import styles from './Main.module.scss';
-import { Dispatch, SetStateAction } from 'react';
 import ThemeSelector from '../ThemeSelector/ThemeSelector';
 
-type Props = {
-    currentTheme: string;
-    setCurrentTheme: Dispatch<SetStateAction<string>>;
-};
-
-export default function Main({ currentTheme, setCurrentTheme }: Props) {
+export default function Main() {
     return (
         <main className={styles.main}>
-            <ThemeSelector currentTheme={currentTheme} setCurrentTheme={setCurrentTheme} />
+            <ThemeSelector />
         </main>
     );
 }
