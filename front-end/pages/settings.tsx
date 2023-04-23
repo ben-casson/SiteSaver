5a30867139e7e5194de0e2249f4d8ade299fd9ff
Ben Casson <bcasson11@gmail.com>
2023-04-22 20:29:49 -0500

    Create settings page

---
+++ front-end/pages/settings.tsx
@@ -0,0 +1,21 @@
+import styles from '@/styles/settings.module.scss';
+import Head from 'next/head';
+import Layout from '@/components/layout/layout';
+import ThemeSelector from '@/components/ThemeSelector/ThemeSelector';
+import Header from '@/components/Header/Header';
+
+export default function Settings() {
+    return (
+        <Layout>
+            <Head>
+                <title>SiteSaver</title>
+                <meta name='description' content='A better way to bookmark' />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <Header />
+            <h1 className={styles.heading}>Settings</h1>
+            <ThemeSelector />
+        </Layout>
+    );
+}
