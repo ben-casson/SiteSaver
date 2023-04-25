9b6a16c0a77ba581738a5eb908625b532dee9af8
Ben Casson <bcasson11@gmail.com>
2023-04-25 16:56:12 -0500

    Add Bookmarks and Tags sections

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -5,6 +5,9 @@ import Layout from '@/components/layout/layout';
 import Header from '@/components/Header/Header';
 import Main from '@/components/Main/Main';
 import useThemeStore from '@/lib/themeStore';
+import Tags from '@/components/Tags/Tags';
+import SortSelect from '@/components/SortSelect/SortSelect';
+import AddBookmarkButton from '@/components/AddBookmarkButton/AddBookmarkButton';
 
 export default function Home() {
     useEffect(() => {
@@ -25,10 +28,28 @@ export default function Home() {
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
-            <Header />
+            <Header heading='Bookmarks' />
             <Main>
-                <h1 className={styles.heading}>Bookmarks</h1>
-                
+                <div className={styles.container}>
+                    <section className={styles.bookmarksSection}>
+                        <h2 className={styles.heading}>Bookmarks</h2>
+                        <div className={styles.inputsContainer}>
+                            <div className={styles.infoIcon}>i</div>
+                            <input
+                                className={styles.bookmarkSearch}
+                                type='search'
+                                name='bookmarkSearch'
+                                id='bookmarkSearch'
+                            />
+                            <SortSelect />
+                            <div className={styles.sortDirection}>&darr;</div>
+                            <AddBookmarkButton />
+                        </div>
+                    </section>
+                    <aside className={styles.tagsContainer}>
+                        <Tags></Tags>
+                    </aside>
+                </div>
             </Main>
         </Layout>
     );
