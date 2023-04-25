b3a4f5f6263bfe9792670fb5c8bd5f97e4905c62
Ben Casson <bcasson11@gmail.com>
2023-04-25 16:56:32 -0500

    Add heading

--- front-end/pages/settings.tsx
+++ front-end/pages/settings.tsx
@@ -9,14 +9,14 @@ export default function Settings() {
     return (
         <Layout>
             <Head>
-                <title>SiteSaver</title>
+                <title>Settings</title>
                 <meta name='description' content='A better way to bookmark' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
-            <Header />
+            <Header heading='Settings' />
             <Main>
-                <h1 className={styles.heading}>Settings</h1>
+                <h2 className={styles.heading}>Settings</h2>
                 <ThemeSelector />
             </Main>
             
